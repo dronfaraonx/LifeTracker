ec976679798622def301153deb7424b047d8854d
Kirill <83719961+dronfaraonx@users.noreply.github.com>
2024-11-01 01:53:19 +0700

    added table

--- server/db/models/plant.js
+++ server/db/models/plant.js
@@ -9,10 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({Category, Cart}) {
+    static associate({Category, Cart, Order}) {
       this.belongsTo(Category, {foreignKey: "category_id"})
       this.hasMany(Cart, {foreignKey: 'plant_id'})
-      this.hasMany(Order, { foreignKey: "plant_id" })
+      // this.hasMany(Order, { foreignKey: "plant_id" })
     }
   }
   Plant.init({
--- server/db/models/user.js
+++ server/db/models/user.js
@@ -9,9 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({Order}) {
-      this.hasMany(Order, {foreignKey : 'user_id'})
-    }
+    static associate({Cart, Order}) {
+      this.hasMany(Cart, {foreignKey : 'user_id'})
+      this.hasMany(Order, {foreignKey : 'user_id'})    }
   }
   User.init({
     name: DataTypes.STRING,
